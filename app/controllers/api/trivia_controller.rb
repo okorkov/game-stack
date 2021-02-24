6ac1a1a6ece9e0773882ea5c1916e8fd989d00c0
okorkov <alex.okorkov@gmail.com>
2021-02-24 14:04:35 -0800

    controller changed from backend/main to api/trivia, routes updated

--- app/controllers/api/trivia_controller.rb
+++ app/controllers/api/trivia_controller.rb
@@ -1,4 +1,4 @@
-class Backend::MainController < ApplicationController
+class Api::TriviaController < ApplicationController
 
   def easy_question
     render json: Trivium.get_questions_by_difficulty('easy')
--- config/routes.rb
+++ config/routes.rb
@@ -2,12 +2,10 @@ Rails.application.routes.draw do
 
   root to: 'frontend/welcome#index'
 
-  namespace :backend do
-    get 'main/easy_question'
-    get 'main/medium_question'
-    get 'main/hard_question'
+  namespace :api do
+    get 'trivia/easy_question'
+    get 'trivia/medium_question'
+    get 'trivia/hard_question'
   end
-  namespace :frontend do
-    # get 'welcome/index'
-  end
+
 end
