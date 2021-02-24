00012ce809e79b033caac6db5329e1a09f56d265
okorkov <alex.okorkov@gmail.com>
2021-02-24 15:35:10 -0800

    page refresh when click on logo

--- app/javascript/packs/app.js
+++ app/javascript/packs/app.js
@@ -3,19 +3,26 @@ const app = {
 
   init: function() {
     document.getElementById('trivia').addEventListener('click', app.openTrivia)
+    document.getElementById('logo').addEventListener('click', app.logoClick)
+  },
+
+  logoClick: function() {
+    location.reload();
   },
 
   openTrivia: function() {
     document.getElementById('carousel').remove();
     let canvas = document.createElement('canvas');
     canvas.width = innerWidth - 150;
-    canvas.height = 500;
+    canvas.height = 650;
     document.getElementById('carousel-parent').appendChild(canvas);
     let ctx = canvas.getContext('2d');
-    // ctx.fillStyle = 'black';
-    // ctx.fill();
+    ctx.beginPath();
+    ctx.rect(0, 0, canvas.width, canvas.height);
+    ctx.fillStyle = "black";
+    ctx.fill();
   }
-  
+
 }
 
 
