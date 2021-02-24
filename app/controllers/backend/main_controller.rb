0c812c43f80315fbf34b93a26d375719704f0772
okorkov <alex.okorkov@gmail.com>
2021-02-23 17:36:17 -0800

    added back end api for easy, medium and hard questions

--- app/controllers/backend/main_controller.rb
+++ app/controllers/backend/main_controller.rb
@@ -1,4 +1,18 @@
 class Backend::MainController < ApplicationController
-  def index
+
+  def easy_question
+    question = Trivium.where(difficulty: "easy").limit(1).order("RANDOM()")[0]
+    render json: question
   end
+
+  def medium_question
+    question = Trivium.where(difficulty: "medium").limit(1).order("RANDOM()")[0]
+    render json: question
+  end
+
+  def hard_question
+    question = Trivium.where(difficulty: "hard").limit(1).order("RANDOM()")[0]
+    render json: question
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,11 @@ Rails.application.routes.draw do
   root to: 'frontend/welcome#index'
 
   namespace :backend do
-    get 'main/index'
+    get 'main/easy_question'
+    get 'main/medium_question'
+    get 'main/hard_question'
   end
   namespace :frontend do
-    get 'welcome/index'
+    # get 'welcome/index'
   end
 end
