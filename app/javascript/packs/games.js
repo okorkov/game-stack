2128620da3607b87fde69a332779550245902be5
okorkov <alex.okorkov@gmail.com>
2021-03-03 10:20:29 -0800

    update for heroku

--- app/javascript/packs/games.js
+++ app/javascript/packs/games.js
@@ -4,11 +4,11 @@ const domain = 'https://game-stack.herokuapp.com/'
 
 const scoreTable = `
 <div class='p-5 text-center bg-image' id="flappy-dot-template">
-    <div class='d-flex justify-content-center align-items-center h-100'>
-      <div class='text-white'>
+  <div class='d-flex justify-content-center align-items-center h-100'>
+    <div class='text-white'>
       <div class='row'>
-      <div class='col'>
-      <table class="table table-dark table-striped table-bordered"  id="trivia">
+        <div class='col'>
+        <table class="table table-dark table-striped table-bordered"  id="trivia">
         <thead>
         <h3>Trivia</h3>
           <tr>
@@ -19,9 +19,9 @@ const scoreTable = `
         <tbody id="table-body-trivia">
         </tbody>
       </table>
-      </div>
-      <div class='col'>
-      <table class="table table-dark table-striped table-bordered"  id="flappy-dot">
+    </div>
+    <div class='col'>
+    <table class="table table-dark table-striped table-bordered"  id="flappy-dot">
       <thead>
       <h3>Flappy Dot</h3>
         <tr>
@@ -34,10 +34,10 @@ const scoreTable = `
     </table>
     </div>
     </div>
-      </div>
-    </div>
-    </div>
-  </div>`;
+  </div>
+  </div>
+</div>
+</div>`;
 
 document.addEventListener("DOMContentLoaded", () => {
   document.getElementById('all-scores').addEventListener("click", () => renderPage())
